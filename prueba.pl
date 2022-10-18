04f5f8880fc33f2df3901489c423ea79089acc55
Nicolas Aguilera <nicolas.aguilera.g@usach.cl>
2022-10-18 11:39:06 -0300

    Funciones de pertenencia

--- prueba.pl
+++ prueba.pl
@@ -1,33 +1,50 @@
-%image(Width, Height, )
-
-pixbit-d(Posx, Posy, Bit, Depth, [[Posx, Posy], Bit, Depth]) :- 
-	integer(Posx), 0=<Posx,
-	integer(Posy), 0=<Posy,
+pixbit(PosX, PosY, Bit, Depth, [[PosX, PosY], Bit, Depth]) :- 
+	integer(PosX), 0=<PosX,
+	integer(PosY), 0=<PosY,
 	integer(Bit), (Bit=0 ; Bit=1),
 	integer(Depth), 0=<Depth.
 
-pixrgb-d(Posx, Posy, R, G, B, Depth, [[Posx, Posy], [R, G, B], Depth]) :-
-	integer(Posx), 0=<Posx,
-	integer(Posy), 0=<Posy,
+pixrgb(PosX, PosY, R, G, B, Depth, [[PosX, PosY], [R, G, B], Depth]) :-
+	integer(PosX), 0=<PosX,
+	integer(PosY), 0=<PosY,
 	integer(R), 0=<R, R=<255,
 	integer(G), 0=<G, G=<255,
 	integer(B), 0=<B, B=<255,
 	integer(Depth), 0=<Depth.
 
-pixhex-d(Posx, Posy, Hex, Depth, [[Posx, Posy], Hex, Depth]) :-
-	integer(Posx), 0=<Posx,
-	integer(Posy), 0=<Posy,
-	string(Hex),
+pixhex(PosX, PosY, Hex, Depth, [[PosX, PosY], Hex, Depth]) :-
+	integer(PosX), 0=<PosX,
+	integer(PosY), 0=<PosY,
+	string(Hex), string_length(Hex,7),
 	integer(Depth), 0=<Depth.
-
-getColor(P, Color) :- nth0(1, P, Color).
-
 
 
 % ---------------------------------------------------
 
-
-image(Width, Height, L, [Width, Height, [], L, []]) :-
+image(Width, Height, Pixlist, [Width, Height, [], Pixlist, []]) :-
 	integer(Width), 0=<Width,
 	integer(Height), 0=<Height.
 
+pixlistIsBit([]). 
+pixlistIsBit([FirstPixel|RestPixels]):- 
+	pixbit(_,_,_,_,FirstPixel),
+	pixlistIsBit(RestPixels).
+
+imageIsBitmap([_,_,_,Pixlist,_]):- 
+	pixlistIsBit(Pixlist).
+
+pixlistIsRGB([]). 
+pixlistIsRGB([FirstPixel|RestPixels]):- 
+	pixrgb(_,_,_,_,_,_,FirstPixel),
+	pixlistIsRGB(RestPixels).
+
+imageIsPixmap([_,_,_,Pixlist,_]):- 
+	pixlistIsRGB(Pixlist).
+
+pixlistIsHex([]). 
+pixlistIsHex([FirstPixel|RestPixels]):- 
+	pixhex(_,_,_,_,FirstPixel),
+	pixlistIsHex(RestPixels).
+
+imageIsHexmap([_,_,_,Pixlist,_]):- 
+	pixlistIsHex(Pixlist).
